b9f79ce9ce398a6ec39d9269cd73a19c3097808b
Mikoláš Stuchlík <stuchlej@gmail.com>
2019-06-30 21:01:41 +0200

    Declare my interface

--- Subpackage/CswiftLib/Sources/CswiftLib/include/CswiftLib.h
+++ Subpackage/CswiftLib/Sources/CswiftLib/include/CswiftLib.h
@@ -3,7 +3,52 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
 
-void doStuff();
+enum WeekDay{
+    mon, tue, wen, thu, fri, sat, sun
+};
+
+struct Employee{
+    char *first_name;
+    char *last_name;
+    bool is_married;
+    uint64_t salary;
+
+    uint64_t citizen_id;
+};
+
+typedef bool (*HireEmployee)(struct Employee);
+typedef struct Employee (*FireEmployee)(uint64_t);
+
+struct Company{
+    struct Employee *employees;
+    uint16_t employees_length;
+
+    HireEmployee hire_employee;
+    FireEmployee fire_employee;
+};
+
+union MyProperty{
+    struct Company companyOwned;
+    uint64_t total_debt;
+};
+
+struct MyError{
+    char *description;
+    int code;
+};
+
+void greetMe();
+
+void no_params_no_return();
+int no_params_int_retunr();
+
+struct Employee some_params_struct_return(uint64_t citizen_id);
+uint64_t struct_params_type_return(struct  Employee employee);
+uint64_t struct_pointer_param_type_return(struct Employee *employee_pointer);
+
+void copy_employee_to_heap(struct Employee source, struct Employee **destination);
 
 #endif /* C_SWIFT_LIB_H */
