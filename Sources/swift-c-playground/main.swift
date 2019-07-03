d3adffe017738d3c53f05e1a69c0e7eb2c1a81bb
Mikoláš Stuchlík <stuchlej@gmail.com>
2019-07-03 22:43:11 +0200

    Pass unsafe

--- Sources/swift-c-playground/main.swift
+++ Sources/swift-c-playground/main.swift
@@ -6,10 +6,17 @@ greetMe()
 
 let myName = "Mikoláš"
 //let myCName = myName.utf8CString as ContiguousArray<Int8>
-var myLastName = "Struchlík".toUnsa
+let myLastName = "Stuchlík"
+var array = Array(myLastName.utf8CString)
+let unsafe = UnsafeRawPointer(&array).bindMemory(to: Int8.self, capacity: array.count)
+let mutableUnsafe = UnsafeMutablePointer<Int8>(mutating: unsafe)
 
-let employee = Employee(first_name: myName, last_name: nil, is_married: true, salary: 350_000, citizen_id: 9608117777);
+let employee = Employee(first_name: myName, last_name: mutableUnsafe, is_married: true, salary: 350_000, citizen_id: 9608117777);
 
+print_employee(employee);
+
+employee.last_name?[2] = 100 
+ 
 print_employee(employee);
 
 // Convention C a scope
